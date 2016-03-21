1edc75fcedaa1577aa50791670caf9b2615abe78
gouarin <loic.gouarin@math.u-psud.fr>
2016-03-21 11:50:18 +0100

    fix pep8

--- python/session-2016-03-idf/4_outils/ellipsoides/pySynope/ellipsoides/superellipse.py
+++ python/session-2016-03-idf/4_outils/ellipsoides/pySynope/ellipsoides/superellipse.py
@@ -1,75 +1,91 @@
 # coding: utf8
+"""
+implémentation d'une superellipse
+"""
+
 import math
 
 from ..linspace import linspace
 from ..utils import spe_cos, spe_sin
 
 class Superellipse(object):
-  """
-  définit une superellipse.
+    """
+    définit une superellipse.
 
-  x = rx c(theta, 2/m)
-  y = ry s(theta, 2/m)
+    x = rx c(theta, 2/m)
+    y = ry s(theta, 2/m)
 
-  où 
+    où
 
-  c(theta, m) = sign(cos(theta))|cos(theta)|**m
-  s(theta, m) = sign(sin(theta))|sin(theta)|**m
-  
-  Paramètres
-  ==========
+    c(theta, m) = sign(cos(theta))|cos(theta)|**m
+    s(theta, m) = sign(sin(theta))|sin(theta)|**m
 
-  n : nombre de points de discrétisation en theta
+    Paramètres
+    ==========
 
-  rx : rayon suivant x
+    n : nombre de points de discrétisation en theta
 
-  ry : rayon suivant y
+    rx : rayon suivant x
 
-  m : puissance dans l'expression de la superellipse.
+    ry : rayon suivant y
 
-  Attributs
-  =========
-  n : nombre de points de discrétisation en theta
+    m : puissance dans l'expression de la superellipse.
 
-  rx : rayon suivant x
+    Attributs
+    =========
+    n : nombre de points de discrétisation en theta
 
-  ry : rayon suivant y
+    rx : rayon suivant x
 
-  m : puissance dans l'expression de la superellipse.
+    ry : rayon suivant y
 
-  Méthodes
-  ========
+    m : puissance dans l'expression de la superellipse.
 
-  surface: renvoie les points de la surface.
+    Méthodes
+    ========
 
-  area: renvoie l'aire.    
-  
-  """
+    surface: renvoie les points de la surface.
 
-  def __init__(self, n, rx, ry, m):
-    self.n = n
-    self.rx = rx
-    self.ry = ry
-    self.m = m
+    area: renvoie l'aire.
 
-  def surface(self):
-    phi_list = linspace(0., 2.*math.pi, self.n)
-    x = []
-    y = []
-    for phi in phi_list:
-      x.append(self.rx*spe_cos(phi, 2./self.m))
-      y.append(self.ry*spe_sin(phi, 2./self.m))
-    return x, y
+    """
 
-  @property
-  def area(self):
-    r = 1./self.m
-    return 4**(1-r)*self.rx*self.ry*math.sqrt(math.pi)*math.gamma(1+r)/math.gamma(.5+r)
+    def __init__(self, n, rx, ry, m):
+        self.n = n
+        self.rx = rx
+        self.ry = ry
+        self.m = m
+
+    def surface(self):
+        """
+        retourne les points à la surface de la superellipse
+        """
+        phi_list = linspace(0., 2.*math.pi, self.n)
+        x = []
+        y = []
+        for phi in phi_list:
+            x.append(self.rx*spe_cos(phi, 2./self.m))
+            y.append(self.ry*spe_sin(phi, 2./self.m))
+        return x, y
+
+    @property
+    def area(self):
+        """
+        retourne l'aire de la superellipse
+        """
+        r = 1./self.m
+        return 4**(1-r)*self.rx*self.ry*math.sqrt(math.pi)*math.gamma(1+r)/math.gamma(.5+r)
 
 class Circle(Superellipse):
-  def __init__(self, n, r):
-    super(Circle, self).__init__(n, r, r, 2)
+    """
+    définit un cercle à partir d'une superellipse
+    """
+    def __init__(self, n, r):
+        super(Circle, self).__init__(n, r, r, 2)
 
-  @property
-  def perimeter(self):
-    return 2*math.pi*self.rx
+    @property
+    def perimeter(self):
+        """
+        retourne le préimètre d'un cercle
+        """
+        return 2*math.pi*self.rx
