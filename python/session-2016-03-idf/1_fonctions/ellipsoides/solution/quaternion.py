70aa156134222d91bdbe85108fdae5c7c9c7923b
David Chamont <chamont@in2p3.fr>
2016-03-23 03:48:39 +0100

    review fonctions theorie et TP

--- python/session-2016-03-idf/1_fonctions/ellipsoides/solution/quaternion.py
+++ python/session-2016-03-idf/1_fonctions/ellipsoides/solution/quaternion.py
@@ -2,12 +2,52 @@
 from __future__ import print_function
 import math
 
+# """
+# rotation 2D d'une position à l'aide d'une représentation complexe.
+#
+# Paramètres
+# ==========
+#
+# angle: radians
+#
+# pos : liste de taille 2
+#       point à faire tourner
+#
+# Sortie
+# ======
+#
+# la position tournée
+#
+# """
+def rotate_using_complex(angle, pos):
+  if len(pos) != 2:
+    print("La taille de pos doit être de 2\n")
+    return
+  x = math.cos(angle)
+  y = math.sin(angle)
+  return [x*pos[0] - y*pos[1], x*pos[1] + y*pos[0]]
+
+# """
+# produit hamiltonien de deux quaternions
+#
+# Paramètres
+# ==========
+#
+# q1, q2: deux collections de quatre valeurs.
+#
+# Sortie
+# ======
+#
+# une liste de quatre valeurs
+#
+# """
+
 def hamilton_product(q1, q2):
   return  [ q1[0]*q2[0] - q1[1]*q2[1] - q1[2]*q2[2] - q1[3]*q2[3],
             q1[0]*q2[1] + q1[1]*q2[0] + q1[2]*q2[3] - q1[3]*q2[2],
             q1[0]*q2[2] - q1[1]*q2[3] + q1[2]*q2[0] + q1[3]*q2[1],
             q1[0]*q2[3] + q1[1]*q2[2] - q1[2]*q2[1] + q1[3]*q2[0]]
-  
+
 # """
 # rotation 3D d'une position à l'aide d'une représentation quaternion.
 #
@@ -15,7 +55,7 @@ def hamilton_product(q1, q2):
 # ==========
 #
 # angle : radians
-# 
+#
 # axe : liste de taille 3
 #       axe de rotation
 #
@@ -45,31 +85,6 @@ def rotate_using_quaternion(angle, axe, pos):
   return hamilton_product([w, x, y, z], hamilton_product([0] + pos, [w, -x, -y, -z]))[1:]
 
 
-# """
-# rotation 2D d'une position à l'aide d'une représentation complexe.
-#
-# Paramètres
-# ==========
-#
-# angle: radians
-#
-# pos : liste de taille 2
-#       point à faire tourner
-#
-# Sortie
-# ======
-#
-# la position tournée
-#
-# """
-def rotate_using_complex(angle, pos):
-  if len(pos) != 2:
-    print("La taille de pos doit être de 2\n")
-    return
-  x = math.cos(angle)
-  y = math.sin(angle)
-  return [x*pos[0] - y*pos[1], x*pos[1] + y*pos[0]] 
-
 
 # """
 # demonstration code
@@ -77,4 +92,4 @@ def rotate_using_complex(angle, pos):
 
 print(rotate_using_complex(math.pi/4, [1, 0]))  
 print(rotate_using_quaternion(math.pi/4, [0, 0, 1], [1, 0, 0]))
-print(rotate_using_quaternion_1(math.pi/4, [0, 0, 1], [1, 0, 0]))
+print(rotate_using_quaternion(math.pi/4, [0, 0, 1], [1, 0, 0]))
